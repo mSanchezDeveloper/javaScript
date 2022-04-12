4d516a1240931a428e81fb8162ea90f43d82cdd4
Marcelo Sanchez <marcelo.s44@gmail.com>
2022-04-11 19:31:00 -0500

    Clase DOM

--- javaScript/js/workHomeThree.js
+++ javaScript/js/workHomeThree.js
@@ -1,5 +1,18 @@
+//meses
 let months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-for (let i = 0; i < months.length; i++) {
-    document.write(`${months[i]} <br>`);
+function printMonths(months) {
+    for (let i = 0; i < months.length; i++) {
+        document.write(`${months[i]} <br>`);
+    }
+}
+
+//Function par o impar
+function parImpar() {
+    let num = parseInt(prompt("Ingrese un numero"));
+    if (num % 2 == 0) {
+        document.write(`</br>El numero ${num}, es par.`);
+    } else {
+        document.write(`</br>El numero ${num}, es impar.`);
+    }
 }
